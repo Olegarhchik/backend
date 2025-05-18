16cbd4590fd89676dbbf39b9921addba0c772396
Olegarhchik <atabaev.o.k.04@gmail.com>
2025-05-18 17:05:38 +0300

    Restored previous task

---
+++ 6/go/user/info.go
@@ -0,0 +1,394 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"html/template"
+	"net/http"
+	"regexp"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func checkInfo(info Info) Errors {
+	errors := Errors{}
+
+	pattern := `^([А-Я][а-я]+ ){2}[А-Я][а-я]+$`
+	re := regexp.MustCompile(pattern)
+
+	if info.FullName == "" {
+		errors.FullName = "Заполните поле"
+	} else if !re.MatchString(info.FullName) {
+		errors.FullName = "ФИО должно содержать только буквы русского алфавита"
+	}
+
+	pattern = `^(\+7|8)\d{10}$`
+	re = regexp.MustCompile(pattern)
+
+	if info.Phone == "" {
+		errors.Phone = "Заполните поле"
+	} else if !re.MatchString(info.Phone) {
+		errors.Phone = "Телефон должен содержать 11 цифр, начинающихся с +7 или 8"
+	}
+
+	pattern = `^[A-Za-z][\w\.]+@\w+\.[a-z]+$`
+	re = regexp.MustCompile(pattern)
+
+	if info.Email == "" {
+		errors.Email = "Заполните поле"
+	} else if !re.MatchString(info.Email) {
+		errors.Email = "Email может содержать только буквы латинского алфавита, цифры, знаки . и один знак @"
+	}
+
+	if info.Birthdate == "" {
+		errors.Birthdate = "Заполните поле"
+	}
+
+	if info.Gender == "" {
+		errors.Gender = "Выберите пол"	
+	}
+
+	if len(info.ProgLang) == 0 {
+		errors.ProgLang = "Выберите хотя бы один язык программирования"
+	}
+
+	if info.Bio == "" {
+		errors.Bio = "Заполните поле"
+	} 
+	
+	return errors
+}
+
+func (response InfoResp) IsValid() bool {
+	errors := response.Errors
+
+	return errors.FullName == "" && errors.Phone == "" && errors.Email == "" && errors.Birthdate == "" && errors.Gender == "" && errors.ProgLang == "" && errors.Bio == ""
+}
+
+func isAuthorized(r *http.Request, email string) bool {
+	cookie, err := r.Cookie("accessToken")
+
+	if err != nil {
+		return false
+	}
+
+	token, err := jwt.ParseWithClaims(cookie.Value, &Payload{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte("access-token-secret-key"), nil
+	})
+
+	if err != nil || !token.Valid {
+		return false
+	}
+
+	payload, _ := token.Claims.(*Payload)
+
+	return payload.Email == email
+}
+
+func getIdByEmail(email string) (string, error) {
+	id := ""
+
+	db, err := sql.Open("mysql", "u68861:1067131@/u68861")
+
+	if err != nil {
+		return id, err
+	}
+
+	defer db.Close()
+
+	sel, err := db.Query(fmt.Sprintf(`
+		SELECT Login
+		FROM User
+		WHERE Email = '%s'
+	`, email))
+
+	if err != nil {
+		return id, err
+	}
+
+	defer sel.Close()
+
+	for sel.Next() {
+		err := sel.Scan(&id)
+
+		if err != nil {
+			return id, err
+		}
+	}
+
+	return id, nil
+}
+
+func updateCol(column string, newValue string, IDName string, ID string, table string) error {
+	db, err := sql.Open("mysql", "u68861:1067131@/u68861")
+
+	if err != nil {
+		return err
+	}
+
+	defer db.Close()
+
+	_, err = db.Exec(fmt.Sprintf(`
+		UPDATE %s
+		SET %s = '%s'
+		WHERE %s = '%s';
+	`, table, column, newValue, IDName, ID))
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func getPLID(PLName string) (string, error) {
+	PLID := ""
+
+	db, err := sql.Open("mysql", "u68861:1067131@/u68861")
+
+	if err != nil {
+		return PLID, err
+	}
+
+	defer db.Close()
+
+	sel, err := db.Query(fmt.Sprintf(`
+		SELECT ProgLangID
+		FROM ProgLang
+		WHERE Name = '%s';
+	`, PLName))
+
+	if err != nil {
+		return PLID, err
+	}
+
+	defer sel.Close()
+
+	for sel.Next() {
+		err := sel.Scan(&PLID)
+
+		if err != nil {
+    		return PLID, err
+  		}
+	}
+
+	return PLID, nil
+}
+
+func insertPL(applID string, PLName string) error {
+	PLID, err := getPLID(PLName)
+
+	if err != nil {
+		return err
+	}
+
+	db, err := sql.Open("mysql", "u68861:1067131@/u68861")
+
+	if err != nil {
+		return err
+	}
+
+	defer db.Close()
+
+	return insValues("Abilities", "ApplicationID, ProgLangID", applID, PLID)
+}
+
+func deletePL(applID string, PLName string) error {
+	PLID, err := getPLID(PLName)
+
+	if err != nil {
+		return err
+	}
+
+	db, err := sql.Open("mysql", "u68861:1067131@/u68861")
+
+	if err != nil {
+		return err
+	}
+
+	_, err = db.Exec(fmt.Sprintf(`
+		DELETE FROM Abilities
+		WHERE ApplicationID = '%s' AND ProgLangID = '%s';
+	`, applID, PLID))
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func contains(array []string, elem string) bool {
+	for _, v := range array {
+		if v == elem {
+			return true
+		}
+	}
+
+	return false
+}
+
+func updateDB(oldInfo Info, newInfo Info, id string) error {
+	if oldInfo.FullName == "" {
+		err := insValues("Application", "ApplicationID, FullName, PhoneNumber, Email, Birthdate, Gender, Biography", id, newInfo.FullName, newInfo.Phone, newInfo.Email, newInfo.Birthdate, newInfo.Gender, newInfo.Bio)
+
+		if err != nil {
+			return err
+		}
+
+		for _, pl := range newInfo.ProgLang {
+			err := insertPL(id, pl)
+
+			if err != nil {
+				return err
+			}
+		}
+
+		return nil
+	}
+
+	if oldInfo.FullName != newInfo.FullName {
+		err := updateCol("FullName", newInfo.FullName, "ApplicationID", id, "Application")
+
+		if err != nil {
+			return err
+		}
+	}
+
+	if oldInfo.Phone != newInfo.Phone {
+		err := updateCol("PhoneNumber", newInfo.Phone, "ApplicationID", id, "Application")
+
+		if err != nil {
+			return err
+		}
+	}
+
+	if oldInfo.Birthdate != newInfo.Birthdate {
+		err := updateCol("Birthdate", newInfo.Birthdate, "ApplicationID", id, "Application")
+
+		if err != nil {
+			return err
+		}
+	}
+
+	if oldInfo.Gender != newInfo.Gender {
+		err := updateCol("Gender", newInfo.Gender, "ApplicationID", id, "Application")
+
+		if err != nil {
+			return err
+		}
+	}
+
+	if oldInfo.Bio != newInfo.Bio {
+		err := updateCol("Biography", newInfo.Bio, "ApplicationID", id, "Application")
+
+		if err != nil {
+			return err
+		}
+	}
+
+	for _, pl := range newInfo.ProgLang {
+		if (!contains(oldInfo.ProgLang, pl)) {
+			err := insertPL(id, pl)
+
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	for _, pl := range oldInfo.ProgLang {
+		if (!contains(newInfo.ProgLang, pl)) {
+			err := deletePL(id, pl)
+
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func applyChanges(info Info, id string) error {
+	oldInfo, err := getUser(id)
+
+	if err != nil {
+		return err
+	}
+
+	err = updateDB(oldInfo, info, id)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func saveInfoHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("info.html")
+
+	if err != nil {
+		fmt.Fprintf(w, "Ошибка при работе с шаблоном: %v", err)
+		return
+	}
+
+	response := InfoResp{}
+
+	if r.Method == http.MethodPost {
+		err := r.ParseForm()
+
+		if err != nil {
+			fmt.Fprintf(w, "Ошибка при работе с формой: %v", err)
+			return
+		}
+
+		info := Info{
+			FullName: r.FormValue("full_name"),
+			Phone: r.FormValue("phone"),
+			Email: r.FormValue("email"),
+			Birthdate: r.FormValue("birthdate"),
+			Gender: r.FormValue("gender"),
+			ProgLang: r.PostForm["prog_lang[]"],
+			Bio: r.FormValue("bio"),
+		}
+		id, err := getIdByEmail(info.Email)
+
+		if err != nil {
+			fmt.Fprintf(w, "Ошибка при работе с базой данных: %v", err)
+			return
+		}
+
+		errors := checkInfo(info)
+		response = InfoResp{info, errors, false}
+
+		if !isAuthorized(r, info.Email) {
+			tmpl, err := template.ParseFiles("login.html")
+
+			if err != nil {
+				fmt.Fprintf(w, "Ошибка при работе с шаблоном: %v", err)
+				return
+			}
+
+			response := LoginResp{
+				User: User{},
+				Error: "",
+				Type: "Unauthorized",
+			}
+
+			tmpl.Execute(w, response)
+			return
+		}
+
+		if !response.IsValid() {
+			tmpl.Execute(w, response)
+			return
+		}
+		
+		applyChanges(info, id)
+		response.Saved = true
+	}
+
+	tmpl.Execute(w, response)
+}
---
+++ 6/go/user/register.go
@@ -0,0 +1,244 @@
+package main
+
+import (
+	"crypto/sha256"
+	"database/sql"
+	"fmt"
+	"html/template"
+	"math/rand"
+	"net/http"
+	"regexp"
+	"strings"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type (
+	RegisterResp struct {
+		Error string
+	}
+
+	LoginResp struct {
+		User User
+		Error string
+		Type string
+	}
+
+	User struct {
+		Login string
+		Password string
+		Email string
+	}
+)
+
+func insValues(table string, cols string, values ...string) error {
+	db, err := sql.Open("mysql", "u68861:1067131@/u68861")
+
+	if err != nil {
+		return err
+	}
+
+	defer db.Close()
+
+	for i, value := range values {
+		values[i] = "'" + value + "'"
+	}
+
+	_, err = db.Exec(fmt.Sprintf(`
+		INSERT INTO %s(%s)
+		VALUES (%s)
+	`, table, cols, strings.Join(values, ", ")))
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func validateEmail(email string) string {
+	pattern := `^[A-Za-z][\w\.]+@\w+\.[a-z]+$`
+	re := regexp.MustCompile(pattern)
+
+	if re.MatchString(email) {
+		return ""
+	} else {
+		return "Неверный формат email"
+	}
+}
+
+func findUserByEmail(email string) (User, error) {
+	user := User{}
+
+	db, err := sql.Open("mysql", "u68861:1067131@/u68861")
+
+	if err != nil {
+		return user, err
+	}
+
+	defer db.Close()
+
+	sel, err := db.Query(fmt.Sprintf("SELECT Login, Password, Email FROM User WHERE Email='%s'", email))
+
+	if err != nil {
+		return user, err
+	}
+
+	defer sel.Close()
+
+	for sel.Next() {
+		err := sel.Scan(&user.Login, &user.Password, &user.Email)
+
+		if err != nil {
+			return user, err
+		}
+	}
+
+	return user, nil
+}
+
+func userExists(email string) (string, error) {
+	user, err := findUserByEmail(email)
+
+	if err != nil {
+		return "", err
+	}
+
+	if user == (User{}) {
+		return "", nil
+	} else {
+		return "Пользователь с таким email уже существует", nil
+	}
+}
+
+func generatePassword(length int) string {
+	chars := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{}|;':\",./<>?`~"
+	password := ""
+
+	for i := 0; i < length; i++ {
+		password += string(chars[rand.Intn(len(chars))])
+	}
+
+	return password
+}
+
+func increaseByOne(str string) string {
+	digits := "0123456789"
+	res := ""
+
+	p := len(str) - 1
+
+	for str[p] == '9' {
+		res += "0"
+		p--
+	}
+
+	q := strings.Index(digits, string(str[p])) + 1
+	res = string(digits[q]) + res
+
+	return str[:p] + res
+}
+
+func generateLogin() (string, error) {
+	db, err := sql.Open("mysql", "u68861:1067131@/u68861")
+
+	if err != nil {
+		return "", err
+	}
+
+	defer db.Close()
+
+	lastLogin := "0"
+	sel, err := db.Query("SELECT Login FROM User ORDER BY Login DESC LIMIT 1")
+
+	if err != nil {
+		return "", err
+	}
+
+	defer sel.Close()
+
+	for sel.Next() {
+		err := sel.Scan(&lastLogin)
+
+		if err != nil {
+			return "", err
+		}
+	}
+
+	return increaseByOne("u" + strings.Repeat("0", 6 - len(lastLogin)) + lastLogin), nil
+}
+
+func createUser(email string) (User, error) {
+	login, err := generateLogin()
+
+	if err != nil {
+		return User{}, err
+	}
+
+	password := generatePassword(8)
+
+	return User{login, password, email}, nil
+}
+
+func registerHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("register.html")
+
+	if err != nil {
+		fmt.Fprintf(w, "Ошибка при работе с шаблоном: %v", err)
+		return
+	}
+
+	response := RegisterResp{}
+
+	if r.Method == http.MethodPost {
+		email := r.FormValue("email")
+
+		// проверка email
+		response.Error = validateEmail(email)
+
+		if response.Error == "" {
+			response.Error, err = userExists(email)
+		
+			if err != nil {
+				fmt.Fprintf(w, "Ошибка при заполнении формы регистрации: %v", err)
+				return
+			}
+		}
+
+		if response.Error != "" {
+			tmpl.Execute(w, response)
+			return
+		}
+
+		response := LoginResp{}
+
+		// создание пользователя
+		response.User, err = createUser(email)
+
+		if err != nil {
+			fmt.Fprintf(w, "Ошибка при создании пользователя: %v", err)
+			return
+		}
+
+		// добавление пользователя в базу данных
+		err = insValues("User", "Password, Email", fmt.Sprintf("%x", sha256.Sum256([]byte(response.User.Password))), response.User.Email)
+		response.Type = "postRegister"
+
+		if err != nil {
+			fmt.Fprintf(w, "Ошибка при добавлении пользователя в базу данных: %v", err)
+			return
+		}
+
+		tmpl, err = template.ParseFiles("login.html")
+
+		if err != nil {
+			fmt.Fprintf(w, "Ошибка при работе с шаблоном: %v", err)
+			return
+		}
+
+		tmpl.Execute(w, response)
+		return
+	}
+
+	tmpl.Execute(w, response)
+}
